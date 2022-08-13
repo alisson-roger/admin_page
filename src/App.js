f615890b4920bc1cf3d1ebf4e563e51b9377ad14
Alisson Roger Barbosa Lima <50409048+alisson-roger@users.noreply.github.com>
2022-08-13 16:41:42 -0300

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import Topbar from "./components/topbar/Topbar";
-import Sidebar from "./components/sidebar/Sidebar";
-import './app.css';
 import Home from "./pages/home/Home";
 
 function App() {
-  return (
-    <div >
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-
-
-        <Home />
-
-      </div>
-    </div>
-  );
+  return <div>
+    <Home />
+  </div>
+    ;
 }
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,7 @@
-import Chart from '../../components/chart/Chart';
-import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
-import './home.css';
-import { userData } from '../../utils/data';
-import WidgetLg from '../../components/widgetLg/WidgetLg';
-import WidgetSm from '../../components/widgetSm/WidgetSm';
+import './home.scss';
 
 const Home = () => {
-  return (
-    <div className="home">
-      <FeaturedInfo />
-      <Chart
-        data={userData}
-        title="User Analytics"
-        grid
-        dataKey="Active User"
-      />
-      <div className="homeWidget">
-        <WidgetSm />
-        <WidgetLg />
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
