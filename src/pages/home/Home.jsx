225d66ca4a532a3aa2aaaef2a4ca0ca7ddb612d9
Alisson Roger Barbosa Lima <50409048+alisson-roger@users.noreply.github.com>
2022-08-13 18:39:42 -0300

    sidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,15 @@
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './home.scss';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      {/* <Navbar /> */}
+      <Sidebar />
+      <div className="homeContainer">container</div>
+    </div>
+  );
 };
 
 export default Home;
